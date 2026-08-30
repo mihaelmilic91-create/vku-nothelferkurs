96a09a42666e276f58230adde0dfb6426fc0bd29
Mihael Milic <mihael.milic.91@gmail.com>
2026-08-30 13:03:59 +0200

    Add real Anbieter data (SG, ZH, BE, AG, LU, BS) recherchiert, unbeanspruchte Eintraege

---
+++ supabase/migrations/20260830130000_real_anbieter_daten.sql
@@ -0,0 +1,47 @@
+-- Ersetzt die Platzhalter-Beispielanbieter durch recherchierte, echte Anbieter.
+-- Beispieldaten bleiben in der DB (inaktiv), damit nichts verloren geht, aber
+-- sie erscheinen nicht mehr im öffentlichen Verzeichnis.
+
+UPDATE public.anbieter
+SET status = 'inaktiv'
+WHERE slug IN (
+  'fahren-verstand-zuerich-ost',
+  'erste-hilfe-akademie',
+  'fuehrerschein-paket-zuerich-nord',
+  'vku-zentrum-winterthur',
+  'nothelferkurs-bern-west',
+  'fahrschule-linden',
+  'verkehrskunde-basel',
+  'erste-hilfe-luzern',
+  'vku-punkt-st-gallen',
+  'premiers-secours-geneve',
+  'sensibilisation-lausanne',
+  'fahrschule-aarau-mitte',
+  'primo-soccorso-lugano',
+  'vku-zuerich-west',
+  'nothelfer-plus-zuerichsee'
+);
+
+INSERT INTO public.anbieter
+  (name, slug, adresse, plz, ort, kanton, kurstyp, preis_chf, sprache, website_url, kontakt_email, kontakt_telefon, termine_url, status, lat, lng)
+VALUES
+  ('Lumi Drive', 'lumi-drive-st-gallen', 'Bahnhofstrasse 11', '9000', 'St. Gallen', 'SG', 'beide', 190, ARRAY['Deutsch'], 'https://lumidrive.ch', 'info@lumidrive.ch', '079 193 22 07', 'https://lumidrive.ch', 'aktiv', 47.4245, 9.3767),
+
+  ('Fahrschule Hefti (Züri VKU)', 'fahrschule-hefti-zueri-vku', 'Niederdorfstrasse 63', '8001', 'Zürich', 'ZH', 'vku', 99, ARRAY['Deutsch'], 'https://fahrschule-hefti.ch/verkehrskundeunterricht-vku-zuerich/', 'paul-hefti@bluewin.ch', '078 830 55 51', 'https://fahrschule-hefti.ch/verkehrskundeunterricht-vku-zuerich/', 'aktiv', 47.3745, 8.5440),
+
+  ('BLINK Zürich', 'blink-zuerich', 'Bederstrasse 1', '8002', 'Zürich', 'ZH', 'beide', 170, ARRAY['Deutsch'], 'https://blinkdrive.ch/vku-zuerich/', 'mail@blinkdrive.ch', '031 539 10 65', 'https://blinkdrive.ch/vku-zuerich/', 'aktiv', 47.3660, 8.5310),
+
+  ('VKU Bern (Berndrive)', 'vku-bern-berndrive', 'Bubenbergplatz 8', '3011', 'Bern', 'BE', 'beide', 149, ARRAY['Deutsch'], 'https://vkubern.ch/', NULL, '078 237 27 49', 'https://vkubern.ch/', 'aktiv', 46.9480, 7.4390),
+
+  ('BLINK Bern', 'blink-bern', 'Marktgasse 28', '3011', 'Bern', 'BE', 'beide', 170, ARRAY['Deutsch'], 'https://blinkdrive.ch/nothelferkurs-bern/', 'mail@blinkdrive.ch', '031 539 10 65', 'https://blinkdrive.ch/nothelferkurs-bern/', 'aktiv', 46.9483, 7.4470),
+
+  ('BLINK Aarau', 'blink-aarau', 'Industriestrasse 1', '5000', 'Aarau', 'AG', 'beide', 170, ARRAY['Deutsch'], 'https://blinkdrive.ch/nothelferkurs-aargau/', 'mail@blinkdrive.ch', '031 539 10 65', 'https://blinkdrive.ch/nothelferkurs-aargau/', 'aktiv', 47.3900, 8.0480),
+
+  ('BLINK Luzern', 'blink-luzern', 'Habsburgerstrasse 21', '6003', 'Luzern', 'LU', 'beide', 190, ARRAY['Deutsch'], 'https://blinkdrive.ch/vku-luzern/', 'mail@blinkdrive.ch', '031 539 10 65', 'https://blinkdrive.ch/vku-luzern/', 'aktiv', 47.0510, 8.3080),
+
+  ('BLINK Basel', 'blink-basel', 'Wallstrasse 8', '4051', 'Basel', 'BS', 'beide', 170, ARRAY['Deutsch'], 'https://blinkdrive.ch/nothelferkurs-basel/', 'mail@blinkdrive.ch', '031 539 10 65', 'https://blinkdrive.ch/nothelferkurs-basel/', 'aktiv', 47.5540, 7.5870),
+
+  ('Fahrschule Blumer', 'fahrschule-blumer-basel', 'Hochstrasse 4', '4053', 'Basel', 'BS', 'nothelferkurs', 69, ARRAY['Deutsch'], 'https://fahrschule-blumer.ch/kurse/nothelferkurs-basel/', NULL, NULL, 'https://fahrschule-blumer.ch/kurse/nothelferkurs-basel/', 'aktiv', 47.5470, 7.5890),
+
+  ('Fahrschule Z (Stadelhofen)', 'fahrschule-z-stadelhofen', 'Kreuzbühlstrasse 16', '8008', 'Zürich', 'ZH', 'nothelferkurs', 99, ARRAY['Deutsch'], 'https://www.fahrschule-z.ch/nothelferkurs/zuerich-stadelhofen/', NULL, NULL, 'https://www.fahrschule-z.ch/nothelferkurs/zuerich-stadelhofen/', 'aktiv', 47.3630, 8.5560)
+ON CONFLICT (slug) DO NOTHING;
